eca76efed0409cc20d6eb4df94364b0f60a3d04d
SanjayNithish <sanjay24112003@gmail.com>
2024-06-16 20:57:01 +0530

    updated api headers

--- src/api/api.js
+++ src/api/api.js
@@ -5,4 +5,8 @@ const API_URI = "https://blog-app-backend-spea.onrender.com";
 export const api = axios.create({
   baseURL: API_URI,
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
 });
